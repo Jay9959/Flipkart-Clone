428205df51b9feedf7dcbd1934f4e8509a0eae21
Jay9959 <dobariyajay9959@gmail.com>
2025-03-03 16:59:02 +0530

    changed image path

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -1,4 +1,7 @@
 import { Carousel } from "react-bootstrap";
+import image1 from '../image/7f3cde58a30f6024.jpg'
+import image2 from '../image/1ef81f497320ac22.jpeg'
+import image3 from '../image/912c93218db5310a.jpeg'
 
 const Sliders = () => {
     return (
@@ -6,7 +9,7 @@ const Sliders = () => {
             <Carousel.Item>
                 <img
                     className="d-block w-100 rounded shadow-lg"
-                    src="../src/image/7f3cde58a30f6024.jpg"
+                    src={image1}
                     alt="Banner 1"
                 />
             </Carousel.Item>
@@ -14,7 +17,7 @@ const Sliders = () => {
             <Carousel.Item>
                 <img
                     className="d-block w-100 rounded shadow-lg"
-                    src="../src/image/1ef81f497320ac22.jpeg"
+                    src={image2}
                     alt="Banner 2"
                 />
             </Carousel.Item>
@@ -22,7 +25,7 @@ const Sliders = () => {
             <Carousel.Item>
                 <img
                     className="d-block w-100 rounded shadow-lg"
-                    src="../src/image/912c93218db5310a.jpeg"
+                    src={image3}
                     alt="Banner 3"
                 />
             </Carousel.Item>
